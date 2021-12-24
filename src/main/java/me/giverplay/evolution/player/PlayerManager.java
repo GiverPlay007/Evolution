6f8e1b184b5381c91cfadedcefe96c8efec11128
GiverPlay007 <play.giver.007@gmail.com>
2021-12-24 12:45:06 -0300

    User UUID instead player name

--- src/main/java/me/giverplay/evolution/player/PlayerManager.java
+++ src/main/java/me/giverplay/evolution/player/PlayerManager.java
@@ -3,16 +3,18 @@ package me.giverplay.evolution.player;
 import me.giverplay.evolution.Evolution;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.UUID;
 import java.util.logging.Level;
 
 public class PlayerManager {
 
-  private final HashMap<String, YamlConfiguration> dataCache = new HashMap<>();
+  private final HashMap<UUID, YamlConfiguration> dataCache = new HashMap<>();
   private final Evolution plugin;
   private final File playersFolder;
 
@@ -26,29 +28,27 @@ public class PlayerManager {
     }
   }
 
-  public YamlConfiguration getPlayerData(String playerName) {
-    playerName = playerName.toLowerCase();
-
-    if(dataCache.containsKey(playerName)) {
-      return dataCache.get(playerName);
+  public YamlConfiguration getPlayerData(UUID uuid) {
+    if(dataCache.containsKey(uuid)) {
+      return dataCache.get(uuid);
     }
 
-    File playerFile = new File(playersFolder, playerName + ".yml");
+    File playerFile = new File(playersFolder, uuid.toString() + ".yml");
     YamlConfiguration playerData = new YamlConfiguration();
 
     if(!playerFile.exists()) {
-      plugin.getLogger().warning("Player data " + playerName + " does not exists");
+      plugin.getLogger().warning("Player data " + uuid + " does not exists");
       return playerData;
     }
 
     try {
       playerData.load(playerFile);
     } catch (IOException | InvalidConfigurationException e) {
-      plugin.getLogger().log(Level.SEVERE, "Failed to load player data " + playerName, e);
+      plugin.getLogger().log(Level.SEVERE, "Failed to load player data " + uuid, e);
       e.printStackTrace();
     }
 
-    dataCache.put(playerName, playerData);
+    dataCache.put(uuid, playerData);
     return playerData;
   }
 }
