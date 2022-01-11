3d9dad6b92c5f067a1986e297ce8602fc541e199
GiverPlay007 <play.giver.007@gmail.com>
2022-01-10 22:24:46 -0300

    Clear trash every second

--- src/main/java/me/giverplay/evolution/module/modules/trash/TrashModule.java
+++ src/main/java/me/giverplay/evolution/module/modules/trash/TrashModule.java
@@ -1,14 +1,22 @@
 package me.giverplay.evolution.module.modules.trash;
 
-import dev.arantes.inventorymenulib.utils.InventorySize;
 import me.giverplay.evolution.Evolution;
 import me.giverplay.evolution.command.commands.TrashCommand;
 import me.giverplay.evolution.module.EvolutionModule;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.DoubleChest;
+import org.bukkit.craftbukkit.v1_18_R1.CraftServer;
+import org.bukkit.inventory.DoubleChestInventory;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitTask;
 
 public class TrashModule extends EvolutionModule {
+
+  private Inventory trash;
+  private BukkitTask task;
 
   public TrashModule(Evolution evolution) {
     super(evolution, "Trash");
@@ -16,15 +24,46 @@ public class TrashModule extends EvolutionModule {
 
   @Override
   protected void onEnable() {
+    trash = Bukkit.createInventory(null, 54, ChatColor.DARK_RED + "Lixeira");
+    startTask();
     evolution.getCommandHandler().registerCommand(new TrashCommand(this));
   }
 
   @Override
   protected void onDisable() {
     evolution.getCommandHandler().unregisterCommand("trash");
+
+    if(task != null) {
+      task.cancel();
+      task = null;
+    }
+
+    trash = null;
+  }
+
+  private void startTask() {
+    task = Bukkit.getScheduler().runTaskTimer(evolution, this::clearTrash, 0L, 20L);
+  }
+
+  private void clearTrash() {
+    ItemStack item;
+    int next;
+
+    for(int i = 53; i >= 0; i--) {
+      item = trash.getItem(i);
+
+      if(item != null && !item.getType().equals(Material.AIR)) {
+        trash.setItem(i, new ItemStack(Material.AIR));
+        next = i + 9;
+
+        if(next <= 53) {
+          trash.setItem(next, item);
+        }
+      }
+    }
   }
 
   public Inventory getTrash() {
-    return Bukkit.createInventory(null, 54, ChatColor.DARK_RED + "Lixeira");
+    return trash;
   }
 }
